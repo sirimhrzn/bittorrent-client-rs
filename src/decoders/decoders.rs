fad7478d5d5068e63e1aa3cfa0e51a63c6f2fdf3
Shirish Maharjan <sirimhrzn@gmail.com>
2023-12-24 14:47:26 +0545

    cleanup

--- src/decoders/decoders.rs
+++ src/decoders/decoders.rs
@@ -1,27 +1,13 @@
-use clap::builder::ValueParserFactory;
-
-#[derive(Debug)]
-pub enum DecodedList {
-    Int(i64),
-    String(String),
-}
-
 pub struct Decoder {
     encoded_value: String,
-    integers: Vec<(usize, i64, Option<usize>)>,
 }
-
 impl Decoder {
     pub fn new(encoded_value: String) -> Self {
-        Decoder {
-            encoded_value,
-            integers: Vec::new(),
-        }
+        Decoder { encoded_value }
     }
     pub fn decode(&mut self) -> serde_json::Value {
         if self.encoded_value.chars().next().unwrap().is_numeric() {
-            let val = self.decode_bencoded_value();
-            return val;
+            return self.decode_bencoded_value();
         }
         if self.encoded_value.starts_with('l') && self.encoded_value.ends_with('e') {
             let val = self.decode_bencoded_lists();
@@ -38,15 +24,12 @@ impl Decoder {
     }
 
     pub fn decode_bencoded_lists(&mut self) -> Vec<String> {
-        let decoded_ints = self.decode_bencoded_integer();
-        let mut newString: Vec<String> = Vec::new();
         let till_semi = self.length_before_colon() + 1;
         let value_after_semi = &self.encoded_value[till_semi..];
         let mut int_hit = false;
         let mut int_hit_index = 0;
         let mut char_hit_index = 0;
         let mut num: Vec<String> = Vec::new();
-        let mut ints = String::new();
         for (i, char) in value_after_semi.chars().enumerate() {
             if i == value_after_semi.chars().count() - 1 && char_hit_index == 0 {
                 let value = value_after_semi[char_hit_index..i].to_string();
@@ -71,10 +54,8 @@ impl Decoder {
                 continue;
             }
         }
-        dbg!(&num);
         num
     }
-    // make this length till colon
     fn length_before_colon(&mut self) -> usize {
         let semi = self.encoded_value.find(':').unwrap();
         self.encoded_value[0..semi].len()
@@ -84,11 +65,11 @@ impl Decoder {
         let mut current_sequence = String::from("");
         let mut modified = false;
         let mut parsed_ints: Vec<i64> = Vec::new();
+        let mut integers: Vec<(usize, i64, Option<usize>)> = Vec::new();
         let before_semi = self.length_before_colon();
-        dbg!(&before_semi);
         for (i, char) in self.encoded_value.chars().enumerate() {
             if previous_char == "i" && (char.is_numeric() || char == '-') {
-                self.integers.push((
+                integers.push((
                     i - (before_semi + 1),
                     char.to_string().parse::<i64>().unwrap(),
                     None,
@@ -97,8 +78,8 @@ impl Decoder {
                 modified = true;
             }
             if previous_char == "i" && char.to_string().to_lowercase() == "e" && modified {
-                if let Some(last_index) = self.integers.len().checked_sub(1) {
-                    if let Some(data) = self.integers.get_mut(last_index) {
+                if let Some(last_index) = integers.len().checked_sub(1) {
+                    if let Some(data) = integers.get_mut(last_index) {
                         data.2 = Some(i - before_semi);
                     }
                 }
@@ -110,7 +91,6 @@ impl Decoder {
                 previous_char = "i";
             }
         }
-        dbg!(&parsed_ints);
         parsed_ints
     }
 }
